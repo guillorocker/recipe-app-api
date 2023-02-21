dafcb29285da5b18ca9fda1fe289e697622e3cb1
Guillermo Lujan <guille.larenga@gmail.com>
2023-02-21 13:43:27 -0300

    db wait command and tests

--- app/app/tests.py
+++ app/app/tests.py
@@ -9,16 +9,15 @@ from app import calc
 
 class CalcTest(SimpleTestCase):
     """Test the calc module."""
-    
+
     def test_add_numbers(self):
         """Test adding numbers together."""
         res = calc.add(5, 6)
-        
+
         self.assertEqual(res, 11)
-        
+
     def test_subtract_numbers(self):
         """Test substract numbers together."""
         res = calc.subtract(10, 15)
-               
-        self.assertEqual(res, 5)
 
+        self.assertEqual(res, 5)
